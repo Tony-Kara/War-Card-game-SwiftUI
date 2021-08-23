f88a4fc7ec01ec837c6424b4e826ebf4b829d68b
Tony-Kara <eniolaanthony@gmail.com>
2021-08-24 01:09:11 +0600

    Initial Commit

---
+++ War Card game SwiftUI/War_Card_game_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_game_SwiftUIApp.swift
+//  War Card game SwiftUI
+//
+//  Created by mac on 8/24/21.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_game_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
